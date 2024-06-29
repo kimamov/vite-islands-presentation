731a0d7f516c8d71a57f2e5aa4c707b81c8b0fa7
Nyannazz <kantemir.imam@gmail.com>
2024-06-29 12:19:09 +0200

    add visibility check

--- rawdog/islands-raw/src/islands/client.ts
+++ rawdog/islands-raw/src/islands/client.ts
@@ -1,5 +1,23 @@
 import islands from './manifest'
 
+
+
+function visible(element: HTMLElement, offsetPx: number = 0) {
+    return new Promise(function (resolve) {
+        const io = new IntersectionObserver(entries => {
+            for (const entry of entries) {
+                if (entry.isIntersecting) {
+                    io.disconnect()
+                    resolve(true)
+                }
+            }
+        }, {
+            rootMargin: `${offsetPx}px`
+        })
+        io.observe(element)
+    })
+}
+
 class Island extends HTMLElement {
     async connectedCallback() {
         const src = this.getAttribute('src') ?? '';
@@ -9,6 +27,11 @@ class Island extends HTMLElement {
         if (!componentLoader) {
             throw new Error(`Could not find component for ${src}`);
         }
+        if (this.hasAttribute('client:visible')) {
+            await visible(this, 200)
+        }
+
+
         const props = JSON.parse(this.getAttribute('props') ?? '{}');
         const component = (await componentLoader()).default;
         this.appendChild(component(props));
