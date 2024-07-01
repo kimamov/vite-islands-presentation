e34fff19fc20a169a02c18cd7cded30a927aba4b
Nyannazz <kantemir.imam@gmail.com>
2024-07-01 12:42:27 +0200

    readme

--- rawdog/islands-raw/src/islands/VueConcertCalendar.ts
+++ rawdog/islands-raw/src/islands/VueConcertCalendar.ts
@@ -1,5 +1,18 @@
 export default function createCalendar() {
-    const calendar = document.createElement('div');
-    calendar.innerHTML = 'Calendar goes here';
-    return calendar;
+    const styles = `
+        * {
+            outline: 2px solid red;
+        }
+    `
+    const wrapper = document.createDocumentFragment();
+
+    const styleElement = document.createElement('style')
+    styleElement.innerHTML = styles;
+    wrapper.appendChild(styleElement);
+
+    const calendar = document.createElement('div')
+    calendar.innerHTML = "client side rendered calendar"
+    wrapper.appendChild(calendar);
+
+    return wrapper;
 }
--- rawdog/islands-raw/src/islands/client.ts
+++ rawdog/islands-raw/src/islands/client.ts
@@ -18,6 +18,8 @@ function visible(element: HTMLElement, offsetPx: number = 0) {
     })
 }
 
+
+
 class Island extends HTMLElement {
     async connectedCallback() {
         const src = this.getAttribute('src') ?? '';
@@ -27,11 +29,13 @@ class Island extends HTMLElement {
         if (!componentLoader) {
             throw new Error(`Could not find component for ${src}`);
         }
+        // falls die Komponente client:visible hat, warte bis sie 200px vom viewport entfernt ist und lade sie dann.
+
         if (this.hasAttribute('client:visible')) {
             await visible(this, 200)
         }
 
-
+        // hydriere die nun aufn client gerenderte Komponente mit den initialen Daten, vom Server.
         const props = JSON.parse(this.getAttribute('props') ?? '{}');
         const component = (await componentLoader()).default;
         this.appendChild(component(props));
