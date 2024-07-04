27dfb46bc98080580790d734c5b2daac91e8a6c3
Nyannazz <kantemir.imam@gmail.com>
2024-07-04 22:54:42 +0200

    updates

---
+++ rawdog/islands-raw/src/islands/Calendar.vue
@@ -0,0 +1,27 @@
+<script>
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+
+export default {
+    components: {
+        FullCalendar // make the <FullCalendar> tag available
+    },
+    data: function () {
+        return {
+            calendarOptions: {
+                plugins: [dayGridPlugin],
+                initialView: 'dayGridMonth',
+                weekends: false,
+                events: [
+                    { title: 'Meeting', start: new Date() }
+                ]
+            }
+        }
+    }
+}
+</script>
+
+<template>
+    <h1>Demo App</h1>
+    <FullCalendar :options='calendarOptions' />
+</template>
---
+++ rawdog/islands-raw/src/islands/ReactImageSlider.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import ReactDOM from 'react-dom/client'
+
+import ImageGallery from "react-image-gallery";
+// import stylesheet if you're not already using CSS @import
+import "react-image-gallery/styles/css/image-gallery.css";
+
+const images = [
+    {
+        original: "https://picsum.photos/id/1018/1000/600/",
+        thumbnail: "https://picsum.photos/id/1018/250/150/",
+    },
+    {
+        original: "https://picsum.photos/id/1015/1000/600/",
+        thumbnail: "https://picsum.photos/id/1015/250/150/",
+    },
+    {
+        original: "https://picsum.photos/id/1019/1000/600/",
+        thumbnail: "https://picsum.photos/id/1019/250/150/",
+    },
+];
+
+class MyGallery extends React.Component {
+    render() {
+        return <ImageGallery items={images} />;
+    }
+}
+
+export default function reactImageSliderApp() {
+    const wrapper = document.createElement("div");
+    ReactDOM.createRoot(wrapper).render(
+        <MyGallery />
+    )
+    wrapper.innerHtml += "test"
+    return wrapper;
+}
